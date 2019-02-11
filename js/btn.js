209ca5ab57bde8272f1c7686bdb8dc3fc52d7e98
gkfnf <978694661@qq.com>
2019-02-11 08:44:25 +0800

    deleted:    .csscomb.json 	deleted:    .gitignore 	deleted:    2018/google/index.html 	deleted:    2018/xss/index.html 	deleted:    404.html 	deleted:    CNAME 	modified:   categories/index.html 	modified:   categories/index.xml 	new file:   categories/info-gathering/index.html 	renamed:    "categories/\346\270\227\351\200\217\346\265\213\350\257\225/index.xml" -> categories/info-gathering/index.xml 	new file:   categories/info-gathering/page/1/index.html 	new file:   categories/language/index.html 	new file:   categories/language/index.xml 	new file:   categories/language/page/1/index.html 	new file:   categories/sql-injection/index.html 	new file:   categories/sql-injection/index.xml 	new file:   categories/sql-injection/page/1/index.html 	new file:   categories/tools/index.html 	new file:   categories/tools/index.xml 	new file:   categories/tools/page/1/index.html 	deleted:    "categories/web-\345\256\211\345\205\250/index.html" 	deleted:    "categories/web-\345\256\211\345\205\250/index.xml" 	deleted:    "categories/web-\345\256\211\345\205\250/page/1/index.html" 	new file:   categories/xss/index.html 	new file:   categories/xss/index.xml 	new file:   categories/xss/page/1/index.html 	deleted:    "categories/\346\270\227\351\200\217\346\265\213\350\257\225/index.html" 	deleted:    "categories/\346\270\227\351\200\217\346\265\213\350\257\225/page/1/index.html" 	new file:   "categories/\346\274\217\346\264\236\345\210\206\346\236\220/index.html" 	new file:   "categories/\346\274\217\346\264\236\345\210\206\346\236\220/index.xml" 	new file:   "categories/\346\274\217\346\264\236\345\210\206\346\236\220/page/1/index.html" 	new file:   contact/index.html 	deleted:    css/main.min.css 	new file:   css/prism.css 	new file:   css/vec.min.css 	new file:   faq/index.html 	deleted:    font/iconfont.css 	new file:   images/avatar.jpg 	new file:   images/xss/Linux_command.jpg 	new file:   images/xss/web_Architecture.jpg 	new file:   images/xss/web_Front_introduction.jpg 	new file:   img/avatar.png 	modified:   index.html 	modified:   index.xml 	new file:   js/btn.js 	new file:   js/prism.js 	deleted:    js/vendor_gallery.min.js 	deleted:    js/vendor_main.min.js 	deleted:    js/vendor_no_gallery.min.js 	new file:   post/burpsuite/index.html 	new file:   post/google/index.html 	new file:   post/index.html 	new file:   post/index.xml 	new file:   post/page/1/index.html 	new file:   post/programing-language/index.html 	new file:   post/sql_injection/index.html 	new file:   post/vulnerability/index.html 	new file:   post/xss/index.html 	new file:   post/xss_files/1.png 	deleted:    posts/index.html 	deleted:    posts/index.xml 	deleted:    posts/page/1/index.html 	deleted:    posts/xss_eg1.php 	deleted:    robots.txt 	modified:   sitemap.xml 	modified:   tags/index.html 	modified:   tags/index.xml

---
+++ js/btn.js
@@ -0,0 +1,7 @@
+document.getElementById("toc_btn").addEventListener("click", function(){
+    alert("the toc was clicked!")
+});
+
+document.getElementById("bulb_btn").addEventListener("click", function(){
+    alert("the bulb was clicked!")
+});
